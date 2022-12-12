439464c52d71276486e34b93b0e76d2c2dbf48a5
Overbetx2 <120251083+Overbetx2@users.noreply.github.com>
2022-12-12 18:11:53 +0100

    multiply

---
+++ lessons/lesson2.py
@@ -0,0 +1,12 @@
+# Конкатенация строк - это сложение строк
+# Репликация строк - это умножение строки на число
+first_name = "German"
+last_name = "Sorochynskyi"
+sum = first_name + " " + last_name #конкатенация
+multiply = (first_name + " ") * 5
+print("multi:" + multiply, "overbet", sep="*", end="")
+print()
+print(type(sum))
+print(type(multiply))
+
+#homework: повторить уроки / pep8 / github / поработать с математическими операторами ( база 100, 2 )
