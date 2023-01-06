5effb4ac7e0079aedf96f308b5ecb177635e9ed2
Overbetx2 <120251083+Overbetx2@users.noreply.github.com>
2023-01-06 12:23:54 +0100

    proof

--- projects/proof of age.py
+++ projects/proof of age.py
@@ -1,9 +1,6 @@
--your_age = int(input("Сколько вам лет - "))
+your_age = int(input("Сколько вам лет - "))
 
 if  your_age < 18:
     print("Подрасти сынок!")
 else:
     print("Добро пожаловать, раздевайся!")
-
-
-
